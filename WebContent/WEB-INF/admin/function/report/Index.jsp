f4d793d621595296372d7dcbcc6cc4613177a39b
nmsugawara <ssugawara@net-marketing.co.jp>
2014-06-16 22:38:39 +0900

    レポートindexページ追加

---
+++ WebContent/WEB-INF/admin/function/report/Index.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+	request.setCharacterEncoding("UTF-8");
+%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link href="/nmnw/commons/css/bootstrap.min.css" rel="stylesheet">
+<link href="/nmnw/commons/css/style.css" rel="stylesheet">
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="/nmnw/commons/js/bootstrap.min.js"></script>
+<title>No Music No Work | レポートTOP</title>
+</head>
+<body>
+<jsp:include page="/commons/Menu.jsp"/>
+<ul class="unstyled">
+	<li><a href="monthlysummaryreport">月別総合レポート</a></li>
+	<li><a href="possibilityofcancelreport">未発送/発送後8日以内の注文一覧レポート</a></li>
+</ul>
+</body>
+</html>
---
+++ src/com/nmnw/admin/function/report/IndexServlet.java
@@ -0,0 +1,43 @@
+package com.nmnw.admin.function.report;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.nmnw.admin.constant.ConfigConstants;
+import com.nmnw.admin.utility.DateConversionUtility;
+
+@WebServlet(name="admin/report/index", urlPatterns={"/admin/report/index"})
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Construct
+	 */
+	public IndexServlet () {
+		super();
+	}
+
+	@Override
+	protected void doGet (HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		response.setContentType("text/html; charset=UTF-8");
+ 		request.setCharacterEncoding("UTF-8");
+		String page = ConfigConstants.JSP_DIR_REPORT + "Index.jsp";
+		request.getRequestDispatcher(page).forward(request, response);
+	}
+
+	@Override
+	protected void doPost (HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		doGet(request, response);
+	}
+}
